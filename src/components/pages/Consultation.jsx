1a1c60cae1eafc40e4ec8e6740f5c668713ae7e0
Loigi Deliguer <loigideliguer1@gmail.com>
2022-02-05 13:15:50 +0800

    create consultation page

---
+++ src/components/pages/Consultation.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./consultation.css";
+
+export default class Consultation extends Component {
+  render() {
+    return (
+      <div className="consultation">
+        <h1>Consultation</h1>
+      </div>
+    );
+  }
+}
