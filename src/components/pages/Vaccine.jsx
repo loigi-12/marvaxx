9592a2eb4f3e9844b929dd927d19b0015633bb7d
Loigi Deliguer <loigideliguer1@gmail.com>
2022-02-05 13:16:01 +0800

    create vaccine page

---
+++ src/components/pages/Vaccine.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./vaccine.css";
+
+export default class Vaccine extends Component {
+  render() {
+    return <div className="vaccine">Vaccine</div>;
+  }
+}
