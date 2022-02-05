b2503eb664e90eaf65dd82d9b738f5436bbd29d0
Loigi Deliguer <loigideliguer1@gmail.com>
2022-02-05 13:14:47 +0800

    create dashboard page

---
+++ src/components/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./dashboard.css";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard">
+        <h1>Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
