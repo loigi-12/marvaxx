bd75b20c093064c8c31e871551d3465b899c1560
Loigi Deliguer <loigideliguer1@gmail.com>
2022-02-05 13:16:10 +0800

    create messages page

---
+++ src/components/pages/Messages.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./messages.css";
+
+export default class Messages extends Component {
+  render() {
+    return <div className="messages">Messages</div>;
+  }
+}
