da9d0f0446c0bb356c82cb4ec2d368ff33e026d4
Loigi Deliguer <loigideliguer1@gmail.com>
2022-02-05 13:15:15 +0800

    create booking page

---
+++ src/components/pages/Booking.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./booking.css";
+
+class Booking extends Component {
+  render() {
+    return (
+      <div className="booking">
+        <h1>Booking</h1>
+      </div>
+    );
+  }
+}
+
+export default Booking;
