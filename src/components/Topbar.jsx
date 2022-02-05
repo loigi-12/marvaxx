ec7d0907c71c98f066d448237ee8ece40c7da057
Loigi Deliguer <loigideliguer1@gmail.com>
2022-02-05 13:14:14 +0800

    create topbar component

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material/";
+import "./topbar.css";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">MarVaxx</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
